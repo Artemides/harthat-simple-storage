87c2c0080c437057b61d1086d79f009014cd95d9
Artemides <edmpulasky@gmail.com>
2023-05-19 16:53:56 -0500

    verify contract in etherscan

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "dotenv/config";
 import "@nomicfoundation/hardhat-toolbox";
-import "@nomicfoundation/hardhat-verify";
+
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL!;
 const PK = process.env.PRIVATE_KEY!;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
@@ -11,6 +11,7 @@ const config: HardhatUserConfig = {
     sepolia: {
       url: SEPOLIA_RPC_URL,
       accounts: [PK],
+      chainId: 11155111,
     },
   },
   solidity: "0.8.8",
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,13 +1,35 @@
-import { ethers } from "hardhat";
-
+import { ethers, run, network } from "hardhat";
+const SEPOLIA_CHAIN_ID = 11155111;
 const main = async () => {
   const simpleStorageFactory = await ethers.getContractFactory("SimpleStorage");
   console.log("deploying...");
   const SimpleStorage = await simpleStorageFactory.deploy();
   await SimpleStorage.deployed();
   console.log(`Deployed to ${SimpleStorage.address}`);
+  console.log(`network ${JSON.stringify(network.config)}`);
+  if (
+    network.config.chainId === SEPOLIA_CHAIN_ID &&
+    process.env.ETHERSCAN_API_KEY
+  ) {
+    await SimpleStorage.deployTransaction.wait(6);
+    await verify(SimpleStorage.address, []);
+  }
 };
-
+const verify = async (contractAddress: string, args: []) => {
+  try {
+    console.log("Verifying contract");
+    await run("verify:verify", {
+      address: contractAddress,
+      constructorArguments: args,
+    });
+  } catch (error: any) {
+    if (error.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified!");
+    } else {
+      console.log({ error });
+    }
+  }
+};
 main()
   .then(() => process.exit(0))
   .catch((error) => {
