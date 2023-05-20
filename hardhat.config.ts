516d9f0f04ab09134a5f6521f345d1600ec3691d
Artemides <edmpulasky@gmail.com>
2023-05-20 17:37:56 -0500

    customize gas reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,8 @@ import "./tasks/block-number";
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL!;
 const PK = process.env.PRIVATE_KEY!;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY!;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY!;
+
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
   networks: {
@@ -27,6 +29,11 @@ const config: HardhatUserConfig = {
   },
   gasReporter: {
     enabled: true,
+    outputFile: "gas-reporter.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    // token: "SOL",
   },
 };
 
